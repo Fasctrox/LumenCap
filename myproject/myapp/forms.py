90d55c94d040727a81104a93b28dad5b30217453
VictorCesarM <victor.cesar.m@gmail.com>
2016-11-25 08:43:12 -0300

    Lumencap Update

---
+++ myproject/myapp/forms.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+from django.contrib.auth.models import User
+
+class DocumentForm(forms.Form):
+    
+    docfile = forms.FileField(
+        label = 'Subir una fotografia'
+        )
+    
+
---
+++ myproject/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
+from django.contrib import admin
+
+urlpatterns = [
+
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('myproject.myapp.urls')),
+    url(r'^', include('myproject.login.urls')),
+    url(r'^.*$', RedirectView.as_view(url='/feed', permanent=False), name='index')  
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
